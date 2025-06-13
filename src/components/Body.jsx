947af92edf70e0fa4c92e34bee20b372a15ae782
Selvamadhavan <selvamadddy1998@gmail.com>
2025-06-13 19:20:48 +0530

    added react code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,24 @@
 import "../stylesheet/Body.css"
 import TaskCard from "./TaskCard"
+import { useState } from "react";
+import { LocalStorage1} from "./LocalStorage";
 
-export default function Body(){
+export default function Body(props){
+    const [data, setData] = useState({...props.taskData1});
+
+
+    const reloadAllTask = () =>{
+        setData(LocalStorage1().find(x => x.PlannerId === data.PlannerId));
+    }
+
+    const taskCards = () =>{
+        return data.Data.map(x => <TaskCard key = {x.CardId} taskCardData = {x} plannerId = {data.PlannerId} reloadCard={reloadAllTask}/>  );
+    }
+
     return(
-        <div class="bodycontainer">
-            <div class="gridcontainer">
-                <TaskCard />     
-                <TaskCard />  
-                <TaskCard />  
-                <TaskCard />                
+        <div className="bodycontainer">
+            <div className="gridcontainer">
+                {taskCards()}                
             </div>
         </div>
     );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,44 +1,45 @@
-import "../stylesheet/Menu.css"
-import profileimg from "../assets/profile.PNG"
+import "../stylesheet/Menu.css";
+import profileimg from "../assets/profile.PNG";
+import PlannerTitleButton from "./PalnnerTitleButton";
+import { AddNewPlanner } from "./LocalStorage";
 
-export default function Menu(){
+export default function Menu(props){
+
+    const titleButtons = () =>{
+        return props.taskData.map(x => <PlannerTitleButton key={x.PlannerId} data1 = {x} selectTask = {props.setTask} 
+            deletePlanner ={props.deletePlanner}/>);
+    }
+
+    const HandleAddPlanner = () => {
+        AddNewPlanner();
+        props.addPlanner();
+    }
+
     return(
-        <div class="menucontainer">
-            <div class="menutoolcontainer">
-                <div class="menuleft">
-                    <button class = "commonbutton">
-                        <i class="bi bi-house homeicon"></i>
+        <div className="menucontainer">
+            <div className="menutoolcontainer">
+                <div className="menuleft">
+                    <button className = "commonbutton">
+                        <i className="bi bi-house homeicon"></i>
                     </button>        
-                    <a class="name"> My Planner </a>
-                    <button class = "commonbutton">
-                        <i class="bi bi-three-dots"></i>
+                    <a className="name"> My Planner </a>
+                    <button className = "commonbutton">
+                        <i className="bi bi-three-dots"></i>
                     </button>
-                    <button class = "sharebutton">
-                        <i class="bi bi-share"></i> Share
-                    </button>
-                    
+                    <button className = "sharebutton">
+                        <i className="bi bi-share"></i> Share
+                    </button>                    
                 </div>
-                <div class ="menuright">
-                    <i class="bi bi-journal-check notebookicon"></i>
-                    <img class="profile" src={profileimg}></img>
+                <div className ="menuright">
+                    <i className ="bi bi-journal-check notebookicon"></i>
+                    <img className ="profile" src={profileimg}></img>
                 </div>
             </div>
-            <div class = "noteheader">
-                <button class="notebutton notebutton1">
-                    my space
-                    <button class="closebutton">
-                        <i class="bi bi-x-circle"></i>
-                    </button>                   
-                </button>
-                <button class="notebutton">
-                    next week
-                    <button class="closebutton">
-                        <i class="bi bi-x-circle"></i>
-                    </button>
-                </button>
-                <button class="addbutton">
-                    <i class="bi bi-plus-lg"></i>
-                </button>
+            <div className = "noteheader">
+                {titleButtons()}
+                {props.taskData.length <= 10 && <button className="addbutton" onClick = {HandleAddPlanner}>
+                    <i className="bi bi-plus-lg"></i>
+                </button>}
             </div>
         </div>
     );
