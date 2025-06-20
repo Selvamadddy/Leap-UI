45d696f1a3e14a422a3ada83c791ef963d7ed699
Selvamadhavan <selvamadddy1998@gmail.com>
2025-06-20 20:59:30 +0530

    code changes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element = {<Home/>} />
+        <Route path="/" element = {<Login/>} />
         <Route path ="/Activity" element={<Activity/>} />
         <Route path ="/Login" element={<Login/>} />
         <Route path ="/Register" element={<Register/>} />
